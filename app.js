39ff4a05163fa0cfcc9906d105378aa7125c335a
Ben <general@simplexwebsites.com>
2018-12-15 16:32:56 -0800

    Major advancement. Know bugged area has comments noting start and end.

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ const {Builder, By, Key, until} = require('selenium-webdriver'),
 let driver = new Builder().forBrowser('chrome').build();
 
 driver.get('https://supremenewyork.com');
+
+driver.findElement(By.className('shop_link')).click();
+
+//Bugged code starts below.
+let all = driver.findElement(By.xpath("//a[@value='view all']"));
+
+driver.promise.filter(all, (element)=>{
+  return element.isDisplayed();
+}).then((element)=>{
+  element.click();
+});
+//End of bugged code.
