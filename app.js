aca02f380e6227a22cadcb85c858ed98c6ed4598
Ben <general@simplexwebsites.com>
2018-12-20 13:22:52 -0800

    Attempt to fix bug. FAILED.

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let category = null;
 category = "jackets";
 category = category.toLowerCase();
 let item = null;
-item = "GORE-TEX 700-Fill Down Parka";
+item = "Supreme®/Schott® Down Leather Vest Puffy Jacket";
 let color = null;
-color = "Black"
+color = "Silver"
 async function main() {
   try{
     let driver = new Builder().forBrowser('chrome').build();
@@ -19,15 +19,16 @@ async function main() {
     let element1 = await driver.wait(until.elementLocated(By.xpath("//*[contains(text(),'"+category+"')]", 100)));
     await element1.click();
 
-    let itemName = await driver.wait(until.elementLocated(By.xpath("//*[@id='container']/article/div/h1/a")));
-    let itemColor = await driver.wait(until.elementLocated(By.xpath("//*[@id='container']/article/div/p/a")));
+    let itemCount = await driver.wait(until.elementLocated(By.xpath("//*[@id='container']/article",1000)));
+    let itemName = await driver.wait(until.elementLocated(By.xpath("//*[@id='container']/article/div/h1/a",1000)));
+    let itemColor = await driver.wait(until.elementLocated(By.xpath("//*[@id='container']/article/div/p/a",1000)));
 
-    for (var i = 0; i < itemName.length; i++) {
-      if (itemName[i]==item && itemColor[i]==color){
-        findElement(By.xpath("//*[@id='container']/article'"+[i]+"'/div/h1/a")).click();
-      } else {
-        continue;
-      }
+    for (var i = 0; i < itemCount.length; i++) {
+     if ("//*[@id='container']/article/div/h1/a/*[text()[normalize-space(.)='"+[i]+"']]"==item && "//*[@id='container']/article/div/p/a/*[text()[normalize-space(.)='"+[i]+"']]"==color){
+       findElement(By.xpath("//*[@id='container']/article'"+[i]+"'/div/h1/a")).click();
+     } else {
+       continue;
+     }
     }
     //end of get to shop/cat
     //--------------------------------------------------------------
