10cca33c767fa9663a79b29c6b3c1184c602bbc9
Ben <general@simplexwebsites.com>
2018-12-19 18:50:04 -0800

    Add different way of selecting item. Not working.

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ let category = null;
 category = "jackets";
 category = category.toLowerCase();
 let item = null;
-item = "GORE-TEX";
+item = "GORE-TEX 700-Fill Down Parka";
 let color = null;
 color = "Black"
 async function main() {
+  try{
     let driver = new Builder().forBrowser('chrome').build();
 
     driver.get('https://supremenewyork.com');
@@ -17,12 +18,27 @@ async function main() {
     await element0.click();
     let element1 = await driver.wait(until.elementLocated(By.xpath("//*[contains(text(),'"+category+"')]", 100)));
     await element1.click();
+
+    let itemName = await driver.wait(until.elementLocated(By.xpath("//*[@id='container']/article/div/h1/a")));
+    let itemColor = await driver.wait(until.elementLocated(By.xpath("//*[@id='container']/article/div/p/a")));
+
+    for (var i = 0; i < itemName.length; i++) {
+      if (itemName[i]==item && itemColor==color){
+        findElement(By.xpath("//*[@id='container']/article'"+[i]+"'/div/h1/a")).click();
+      } else {
+        continue;
+      }
+    }
     //end of get to shop/cat
     //--------------------------------------------------------------
-    //start of select item (bugged code starts below and ends at end of select item.)
-    let element2 = await driver.wait(until.elementLocated(By.linkText(item,color)));
-    element2.click();
+    //start of select item
+    // let element2 = await driver.wait(until.elementLocated(By.linkText("Lime")));
+    // element2.click();
     //end of select item
+  } catch(err) {
+    console.log(err);
+    driver.quit();
+  }
 }
 
 main();
